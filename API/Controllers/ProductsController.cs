9130fcf30fbf064c3cefcdd0d09de9b1b7b2facf
Mcorduk <om.corduk@gmail.com>
2024-04-14 17:42:40 +0300

    Refactor ListProducts method in ProductsController.cs to include pagination and response headers

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -1,6 +1,8 @@
+using System.Text.Json;
 using API.Data;
 using API.Entities;
 using API.Extensions;
+using API.RequestHelpers;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -11,16 +13,19 @@ namespace API.Controllers
         private StoreContext _context { get; } = context;
 
         [HttpGet]
-        public async Task<ActionResult<List<Product>>> ListProducts(string orderBy, string searchTerm, string brands, string types)
+        public async Task<ActionResult<PagedList<Product>>> ListProducts([FromQuery] ProductParams productParams)
         {
             var query = _context.Products
-            .Sort(orderBy)
-            .Search(searchTerm)
-            .Filter(brands, types)
+            .Sort(productParams.OrderBy)
+            .Search(productParams.SearchTerm)
+            .Filter(productParams.Brands, productParams.Types)
             .AsQueryable();
 
+            var products = await PagedList<Product>.ToPagedList(query, productParams.PageNumber, productParams.PageSize);
 
-            return await query.ToListAsync();
+            Response.AddPaginationHeader(products.MetaData);
+
+            return Ok(products);
         }
 
         [HttpGet("{id}")]
@@ -29,6 +34,14 @@ namespace API.Controllers
             var product = await _context.Products.FindAsync(id);
             if (product == null) return NotFound();
             return Ok(product);
+        }
+
+        [HttpGet("filters")]
+        public async Task<IActionResult> GetFilters()
+        {
+            var brands = await _context.Products.Select(p => p.Brand).Distinct().ToListAsync();
+            var types = await _context.Products.Select(p => p.Type).Distinct().ToListAsync();
+            return Ok(new { brands, types });
         }
 
     }
